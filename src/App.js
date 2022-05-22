3ca6932e1113db091793a0d5b9a060caab07f797
fahadshoukat <fraza6970@gmail.com>
2022-05-22 11:25:04 +0500

    add signup form and firebase config file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./components/signup/SignUp";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+    <Routes>
+      <Route path="signup" element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,19 +3,8 @@ import {BsCartDash} from 'react-icons/bs';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-primary">
-      <div className="container-fluid">
-        <a className="navbar-brand text-light fw-bold fs-3">E-STORE</a>
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-dark" type="submit">
-            Search
-          </button>
-        </form>
+      <div className="container">
+        <a className="navbar-brand text-light fw-bold fs-3">Try Basket</a>
         <div>
           <BsCartDash className="mx-4 text-light" fontSize='32px' />
           <button className="btn btn-light" type="button">
